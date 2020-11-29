24680bbb268a1fcfda3d75758ccaab3a41f254db
page-a <43348218+page-a@users.noreply.github.com>
2020-11-29 21:20:17 +0900

    experience vscode

---
+++ MyCode/0001to0010.py
@@ -0,0 +1,59 @@
+#%%
+import numpy as np
+import pandas as pd
+from konlpy.tag import Okt ; okt = Okt()
+from collections import Counter
+from pathlib import Path
+import argparse
+
+
+
+# FILE_PATH=args.FILE_PATH
+# EXPORT_PATH=args.EXPORT_PATH
+# FREQ_N=args.FREQ_N
+
+def myprint(FILE_PATH,EXPORT_PATH,FREQ_N):
+    print(FILE_PATH,EXPORT_PATH,FREQ_N)
+    print(pd.read_csv(Path(FILE_PATH)))
+    return
+
+#%%
+def f0001(FILE_PATH, EXPORT_PATH, FREQ_N=200):
+    # Read Review File
+    #  FILE_PATH = '../Data/BabyProducts/MilkPowder/분유.csv'
+    review=pd.read_csv(Path(FILE_PATH))
+    N_review = len(review)
+    token_ls = []
+    
+    # for i in range(2080,2083):
+    for i in range(N_review):
+        # print(i)
+        body = review['body'][i]
+        if pd.isna(body):
+            continue
+        word_pos=okt.pos(body, stem=True, norm=True)
+        [token_ls.append(token) for token, pos in word_pos if pos=='Noun']
+    count_obj = Counter(token_ls)
+    count = count_obj.most_common(FREQ_N)
+#%%
+    # save excel
+    temp_dict = dict(count)
+    temp_df=pd.DataFrame({'keyword':temp_dict.keys(), 'count':temp_dict.values()})
+    temp_df.sort_values(by='count',ascending=False,inplace=True)
+#%%
+    temp_df.to_excel(Path(EXPORT_PATH),index=False)
+    return 
+
+# count_200=f0001('../Data/BabyProducts/MilkPowder/분유.csv'
+#                 ,'../Data/BabyProducts/MilkPowder/_분유_count.xlsx'
+#                 ,200)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--FILE_PATH','-in')
+    parser.add_argument('--EXPORT_PATH','-out')
+    parser.add_argument('--FREQ_N','-n', type=int)
+    args = parser.parse_args()
+
+    f0001(args.FILE_PATH,args.EXPORT_PATH,args.FREQ_N)
